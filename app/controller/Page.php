c158b8f281c0837957f44c51e60559e6284bd046
cafecladis <cafecladis@users.noreply.github.com>
2021-08-28 16:07:22 +0200

    Added controller class Page

---
+++ app/controller/Page.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Controller class Page
+ *
+ * @package CafeCladisCMS
+ * @author Frau Cladis <hey@fraucladis.de>
+ * @version 0.0.1
+ * @copyright Copyright (c) 2021, Café Cladis
+ * @license GPL
+ */
+class Page
+{
+    public function home()
+    {
+        echo 'home';
+    }
+}
